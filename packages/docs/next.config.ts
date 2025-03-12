6146d68625b9cba5aae4ec6a6b111982b4d43ac9
Younsang Na <77400131+nayounsang@users.noreply.github.com>
2025-03-12 17:54:54 +0900

    feat: play ground (#50)
    * feat: play ground
    
    * fix: ignore msw/browser in server env of build process
    
    * fix: vercel deploy error because of root dir and pnpm local workspaces
    
    * fix: update lock

--- packages/docs/next.config.ts
+++ packages/docs/next.config.ts
@@ -6,7 +6,17 @@ const withNextra = nextra({
 });
 
 const nextConfig: NextConfig = withNextra({
-  /* config options here */
+  /**
+   * This is required for the docs to use msw-dev-tool in production.
+   * The build works in a node environment, but msw/browser should be used strictly in a browser environment.
+   * However, build checks all browser packages that are statically imported.
+   */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias["msw/browser"] = false;
+    }
+    return config;
+  },
 });
 
 export default nextConfig;
