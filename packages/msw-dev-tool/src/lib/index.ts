65ea88cb96bf77b9ecaecd13c56dca32670989ec
nayounsang <nayounsang722@gmail.com>
2025-03-23 20:27:18 +0900

    fix: remove deprecated featrue export

--- packages/msw-dev-tool/src/lib/index.ts
+++ packages/msw-dev-tool/src/lib/index.ts
@@ -1,6 +1,5 @@
 import { FlattenHandler } from "./type";
-import { initMSWDevTool, setupDevToolWorker } from "./handlerStore";
+import { setupDevToolWorker } from "./handlerStore";
 
 export { FlattenHandler };
-export { initMSWDevTool };
 export { setupDevToolWorker };
