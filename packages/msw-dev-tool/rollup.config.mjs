45653593125359c40a95de271d915e67a69b4ba5
nayounsang <nayounsang722@gmail.com>
2025-04-06 21:41:39 +0900

    fix: remove assert json

--- packages/msw-dev-tool/rollup.config.mjs
+++ packages/msw-dev-tool/rollup.config.mjs
@@ -3,8 +3,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import pkg from "./package.json" assert { type: "json" };
 import postcss from "rollup-plugin-postcss";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
 
 const externalPackages = [
   ...Object.keys(pkg.peerDependencies || {}),
